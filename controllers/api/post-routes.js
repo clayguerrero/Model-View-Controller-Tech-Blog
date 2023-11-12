2f74a0c261fe428316b94e154b0706daae5e4c56
clayg <claytondguerrero@gmail.com>
2023-11-12 16:15:07 -0600

    created post-routes.js and comment-routes.js

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router()
+const { User, Post } = require('../../models')
+
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [{ model: User, attributes: ["username"] }],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+    res.render("homepage", {
+      posts,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "no db found (all post data)" });
+  }
+});
+
+
+module.exports = router
